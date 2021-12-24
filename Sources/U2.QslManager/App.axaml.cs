76bc6c9558d2f025af8d849bcd40beb5867a881b
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2021-12-24 19:28:18 +0200

    Added QslManager project

---
+++ Sources/U2.QslManager/App.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using U2.QslManager.ViewModels;
+using U2.QslManager.Views;
+
+namespace U2.QslManager
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ Sources/U2.QslManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace U2.QslManager.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string TabDesignerTitle => "Designer";
+    }
+}
