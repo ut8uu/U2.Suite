c70e56ec4d904beb38f42da77eea6202b5383898
ut8uu <ut8uu@ukr.net>
2021-12-26 16:30:08 +0200

    Updated demo design template

--- Sources/U2.QslManager/Models/QslCardDesign.cs
+++ Sources/U2.QslManager/Models/QslCardDesign.cs
@@ -9,9 +9,21 @@ namespace U2.QslManager
     {
         public string? DesignName { get; set; }
 
-        public Size CardSizeMM { get; set; }
+        public Dimensions CardSizeMM { get; set; }
         public int DensityDpi { get; set; }
 
         public QslCardElement[]? Elements { get; set; }
+    }
+
+    public sealed class Dimensions
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public Dimensions(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
     }
 }
