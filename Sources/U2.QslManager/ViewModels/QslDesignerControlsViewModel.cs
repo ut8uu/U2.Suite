bdbbfeb20c3535213593d259aff810e0968b89bf
ut8uu <ut8uu@ukr.net>
2021-12-27 23:29:04 +0200

    Removed unnecessary files

--- Sources/U2.QslManager/ViewModels/QslDesignerControlsViewModel.cs
+++ Sources/U2.QslManager/ViewModels/QslDesignerControlsViewModel.cs
@@ -11,17 +11,12 @@ namespace U2.QslManager
 {
     public class QslDesignerControlsViewModel : ViewModelBase
     {
-        private List<TextFieldViewModel>? _fields = new List<TextFieldViewModel>();
-
-        private List<QslCardDesign> _designs = Utilities.GetDesigns();
-
+        
         public QslDesignerControlsViewModel()
         {
             
         }
 
-        public List<TextFieldViewModel>? Fields { get; set; }
         
-        public List<QslCardDesign>? Designs { get; set; }
     }
 }
