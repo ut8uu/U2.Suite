b1409b89e0407f57f4776df5213b4176200c83ab
ut8uu <ut8uu@ukr.net>
2022-01-01 23:48:28 +0200

    Implemented displaying of versions of the libraries.

--- Sources/U2.Resources/Collections/ApplicationNames.cs
+++ Sources/U2.Resources/Collections/ApplicationNames.cs
@@ -6,8 +6,49 @@ namespace U2.Resources
 {
     public static class ApplicationNames
     {
+        public const string SuiteWin = "U2.Suite.exe";
+        public const string SuiteOsx = "U2.Suite";
+        public const string SuiteLInux = "U2.Suite";
         public const string QslManagerWin = "U2.QslManager.exe";
         public const string QslManagerOsx = "U2.QslManager";
         public const string QslManagerLInux = "U2.QslManager";
+
+        public static string ChooseByOS(string win, string osx, string linux)
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.MacOSX:
+                    // returned by SilverLight. .NET Core returns Unix for this OS
+                    throw new NotImplementedException("This OS is not supported.");
+                case PlatformID.Win32S:
+                    // not used anymore
+                    throw new NotImplementedException("This OS is not supported.");
+                case PlatformID.Win32Windows:
+                    throw new NotImplementedException("This OS is not supported.");
+                case PlatformID.Win32NT:
+                    return win;
+                case PlatformID.WinCE:
+                    // not used anymore
+                    throw new NotImplementedException("This OS is not supported.");
+                case PlatformID.Unix:
+                    // MacOS falls here
+                    return osx;
+                case PlatformID.Xbox:
+                    // not used anymore
+                    throw new NotImplementedException("This OS is not supported.");
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public static string GetSuiteAppName()
+        {
+            return ChooseByOS(SuiteWin, SuiteOsx, SuiteLInux);
+        }
+
+        public static string GetQslManagerAppName()
+        {
+            return ChooseByOS(QslManagerWin, QslManagerOsx, QslManagerLInux);
+        }
     }
 }
--- Sources/U2.Suite/ViewModels/AboutFormViewModel.cs
+++ Sources/U2.Suite/ViewModels/AboutFormViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Text;
+using U2.Resources;
 
 namespace U2.Suite
 {
@@ -11,7 +14,17 @@ namespace U2.Suite
         public AboutFormViewModel(AboutFormView aboutFormView)
         {
             this._owner = aboutFormView;
+
+            var currentDirectory = Path.GetDirectoryName(this.GetType().Assembly.Location);
+            var u2SuitePath = Path.Combine(currentDirectory, ApplicationNames.GetSuiteAppName()).Replace("exe", "dll");
+            U2SuiteVersion = Assembly.LoadFile(u2SuitePath).GetName().Version.ToString();
+
+            var u2QslManagerPath = Path.Combine(currentDirectory, ApplicationNames.GetSuiteAppName()).Replace("exe", "dll");
+            U2QslManagerVersion = Assembly.LoadFile(u2QslManagerPath).GetName().Version.ToString();
         }
+
+        public string U2SuiteVersion { get; set; }
+        public string U2QslManagerVersion { get; set; }
 
         public void ShowLicense()
         {
