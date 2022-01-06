537c896c75b70ee8f651dae9e190c236ea2e7e17
ut8uu <ut8uu@ukr.net>
2022-01-07 01:39:16 +0200

    Removed failing references

--- Sources/U2Suite/U2.Suite/ViewModels/AboutFormViewModel.cs
+++ Sources/U2Suite/U2.Suite/ViewModels/AboutFormViewModel.cs
@@ -1,5 +1,4 @@
-﻿using AvaloniaEdit.Editor;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -7,7 +6,6 @@ using System.Reflection;
 using System.Text;
 using U2.Contracts;
 using U2.Core;
-using U2.QslManager;
 using U2.Resources;
 
 namespace U2.Suite
@@ -19,21 +17,7 @@ namespace U2.Suite
         public AboutFormViewModel(AboutFormView aboutFormView)
         {
             this._owner = aboutFormView;
-
-            U2SuiteVersion = new U2SuiteAppVersion().GetAssemblyVersion();
-            U2QslManagerVersion = new QslManagerAppVersion().GetAssemblyVersion();
-            U2CoreVersion = new U2CoreAppVersion().GetAssemblyVersion();
-            U2ContractsVersion = new U2ContractsAppVersion().GetAssemblyVersion();
-            U2ResourcesVersion = new U2ResourcesAppVersion().GetAssemblyVersion();
-            AvaloniaEditorVersion = new AvaloniaEditorAppVersion().GetAssemblyVersion();
         }
-
-        public string U2SuiteVersion { get; set; }
-        public string U2QslManagerVersion { get; set; }
-        public string U2CoreVersion { get; set; }
-        public string U2ContractsVersion { get; set; }
-        public string U2ResourcesVersion { get; set; }
-        public string AvaloniaEditorVersion { get; set; }
 
         public void ShowLicense()
         {
