56d27299cdd1f9258733fd9199b4f1d44ecb1593
ut8uu <ut8uu@ukr.net>
2022-01-09 11:55:16 +0200

    Initial commit

---
+++ Sources/U2Suite/U2.Library/App.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using U2.Library.ViewModels;
+using U2.Library.Views;
+
+namespace U2.Library
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
