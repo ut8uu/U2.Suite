9bdd501a7e7951a8c9594ed32e5ee434711ba5e1
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-01-13 17:15:39 +0200

    Just added the Logger application

---
+++ Sources/U2Suite/U2.Logger/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace U2.Logger.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ Sources/U2Suite/U2.Logger/Views/MainWindow.axaml.cs
@@ -0,0 +1,22 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace U2.Logger.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
