1e75b4ed66da9f3600cf2758952460f8c328fb28
ut8uu <ut8uu@ukr.net>
2022-01-17 00:26:58 +0200

    Added log content window

---
+++ Sources/U2Suite/U2.Logger/ViewModels/LogContentWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace U2.Logger
+{
+    public sealed class LogContentWindowViewModel : ViewModelBase
+    {
+        private readonly LoggerDbContext _dbContext;
+
+        public LogContentWindowViewModel()
+        {
+
+        }
+
+        public string CloseButtonText { get; set; } = "Close";
+
+        public string CallsignColumnHeader { get; set; } = "Callsign";
+        public string TimestampColumnHeader { get; set; } = "Timestamp";
+        public string FrequencyColumnHeader { get; set; } = "Freq";
+        public string ModeColumnHeader { get; set; } = "Mode";
+        public string RstSentColumnHeader { get; set; } = "Snt";
+        public string RstReceivedColumnHeader { get; set; } = "Rcv";
+        public string NameColumnHeader { get; set; } = "Name";
+        public string CommentsColumnHeader { get; set; } = "Comments";
+
+        public LogContentWindowViewModel(LoggerDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public ObservableCollection<LogRecordDbo> FullList { get; set; } = default!;
+        public ObservableCollection<LogRecordDbo> FilteredList { get; set; } = default!;
+
+    }
+}
---
+++ Sources/U2Suite/U2.Logger/Views/LogContentWindow.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace U2.Logger
+{
+    [PropertyChanged.DoNotNotify]
+    public partial class LogContentWindow : Window
+    {
+        private LogContentWindowViewModel _viewModel = default!;
+        private LoggerDbContext _dbContext = default!;
+
+        public LogContentWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            _dbContext = new LoggerDbContext();
+            _viewModel = new LogContentWindowViewModel(_dbContext);
+            DataContext = _viewModel;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
