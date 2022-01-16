c624d234047789d3689af3f4eecdfd46cc462f39
ut8uu <ut8uu@ukr.net>
2022-01-16 17:54:46 +0200

    Added control helper

---
+++ Sources/U2Suite/U2.Logger/ControlHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Xml.Linq;
+
+namespace U2.Logger
+{
+    internal static class ControlHelper
+    {
+        public static ApplicationTextBox TextBoxFromString(string fieldName)
+        {
+            switch (fieldName)
+            {
+                case TextInputPanelViewModel.CallsignTextBox:
+                    return ApplicationTextBox.Callsign;
+                case TextInputPanelViewModel.RstRcvdTextBox:
+                    return ApplicationTextBox.RstReceived;
+                case TextInputPanelViewModel.RstSentTextBox:
+                    return ApplicationTextBox.RstSent;
+                case TextInputPanelViewModel.OperatorTextBox:
+                    return ApplicationTextBox.Operator;
+                case TextInputPanelViewModel.CommentsTextBox:
+                    return ApplicationTextBox.Comments;
+                default:
+                    throw new ArgumentException($"Unrecognized text box '{fieldName}.'");
+            }
+        }
+    }
+}
