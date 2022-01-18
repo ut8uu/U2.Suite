fb23ff8aed6f7cf1e14151881d247758ca2256c3
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-01-18 17:28:59 +0200

    Added conversion helper and its tests

---
+++ Sources/U2Suite/U2.Core/Helpers/ConversionHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using U2.Contracts;
+
+namespace U2.Core
+{
+    public static class ConversionHelper
+    {
+        public static List<RadioBand> AllBands = new List<RadioBand>
+        {
+            new Band160M(),
+        };
+
+        /// <summary>
+        /// Converts given band to its most lower frequency.
+        /// </summary>
+        /// <param name="bandType">A band type to process.</param>
+        /// <returns>Returns the most lower frequency of the given band.</returns>
+        /// <exception cref="ArgumentException">Thrown when Unspecified is passed.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when unknown band is passed.</exception>
+        public static double BandToFrequency(RadioBandType bandType)
+        {
+            if (RadioBandType.Unspecified == bandType)
+            {
+                throw new ArgumentException($"Band {bandType} not specified.");
+            }
+
+            var band = AllBands.FirstOrDefault(b => b.Type == bandType);
+            if (band == null)
+            {
+                throw new ArgumentOutOfRangeException($"Band {bandType} is unknown.");
+            }
+
+            return band.BeginKhz;
+        }
+
+        /// <summary>
+        /// Converts given frequency to an amateur band.
+        /// </summary>
+        /// <param name="frequencyKhz">A frequency in kilohertz.</param>
+        /// <returns>Returns object of type <see cref="RadioBandType"/>.</returns>
+        public static RadioBandType FrequencyToBand(double frequencyKhz)
+        {
+            var band = AllBands.FirstOrDefault(b => b.BeginKhz <= frequencyKhz 
+                                                    && b.EndKhz > frequencyKhz);
+            if (band == null)
+            {
+                return RadioBandType.Unspecified;
+            }
+
+            return band.Type;
+        }
+    }
+
+    public class Band160M : RadioBand
+    {
+        public Band160M()
+        {
+            Name = RadioBandName.B160m;
+            BeginKhz = 1810.0;
+            EndKhz = 2000.0;
+            Group = RadioBandGroup.HF;
+            Type = RadioBandType.B160m;
+            SubBands = new List<SubBand>
+            {
+                new SubBand
+                {
+                    BeginKhz = 1810.0,
+                    EndKhz = 1838.0,
+                    Modes = new RadioMode[] {RadioMode.CW}
+                }
+            };
+        }
+    }
+}
---
+++ Sources/U2Suite/U2.Logger.Tests/ConversionHelperTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using U2.Contracts;
+using U2.Core;
+
+namespace U2.Logger.Tests
+{
+    [TestClass]
+    public class ConversionHelperTests
+    {
+        [TestMethod]
+        [DataRow(RadioBandType.B160m, 1810.0)]
+        public void BandToFreq(RadioBandType type, double expectedFreqKhz)
+        {
+            var freq = ConversionHelper.BandToFrequency(type);
+            Assert.AreEqual(expectedFreqKhz, freq);
+        }
+
+        [TestMethod]
+        [DataRow(1810.0, RadioBandType.B160m)]
+        [DataRow(1999.999, RadioBandType.B160m)]
+        public void FreqToBand_Success(double freqKhz, RadioBandType expectedBand)
+        {
+            var band = ConversionHelper.FrequencyToBand(freqKhz);
+            Assert.AreEqual(expectedBand, band);
+        }
+    }
+}
