018bdc3338534bc07544c30fbf1d7622d88b74d6
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-01-25 15:51:59 +0200

    Added string converters

---
+++ Sources/U2Suite/U2.Core/Converters/ToUppercaseConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Avalonia.Data.Converters;
+
+namespace U2.Core
+{
+    public class ToUpperCaseConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var result = value?.ToString()?.ToUpper(culture);
+            return result;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
