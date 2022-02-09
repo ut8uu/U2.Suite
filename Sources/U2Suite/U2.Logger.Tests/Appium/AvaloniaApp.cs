ac5aa423a425644e782f3ac950aeb9b1d4a9048d
ut8uu <ut8uu@ukr.net>
2022-02-10 00:03:52 +0200

    Added UI tests-related stuff

---
+++ Sources/U2Suite/U2.Logger.Tests/Appium/AvaloniaApp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Headless;
+using Avalonia.Threading;
+using Avalonia;
+using Splat;
+using Avalonia.ReactiveUI;
+
+namespace U2.Logger.Tests
+{
+    public static class AvaloniaApp
+    {
+        /*
+        // DI registrations
+        public static void RegisterDependencies() =>
+            Bootstrapper.Register(Locator.CurrentMutable, Locator.Current);
+        //*/
+
+        // stop app and cleanup
+        public static void Stop()
+        {
+            var app = GetApp();
+            if (app is IDisposable disposable)
+            {
+                Dispatcher.UIThread.Post(disposable.Dispose);
+            }
+
+            Dispatcher.UIThread.Post(() => app.Shutdown());
+        }
+
+        public static LoggerMainWindow GetMainWindow() => (LoggerMainWindow)GetApp().MainWindow;
+
+        public static IClassicDesktopStyleApplicationLifetime GetApp() =>
+            (IClassicDesktopStyleApplicationLifetime)Application.Current.ApplicationLifetime;
+
+        public static AppBuilder BuildAvaloniaApp() =>
+            AppBuilder
+                .Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .UseHeadless(); // note that I run app as headless one
+    }
+}
---
+++ Sources/U2Suite/U2.Logger.Tests/Appium/MainWindowInteraction.feature
@@ -0,0 +1,9 @@
+﻿Feature: MainWindowInteraction
+	A main window of the U2.Logger application
+
+@mytag
+Scenario: Save a QSO
+	Given log is empty
+	When user enters 'UT8UU' in Callsign
+	And user clicks the Save button
+	Then log contains 1 record
