245f8cf687ede8a5c1e532f3f2aa336858e04a12
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-04-10 20:51:47 +0300

    Added unit tests

---
+++ Sources/U2Suite/U2.Logger.Tests/ImportAdifViewModelTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace U2.Logger.Tests
+{
+    [TestClass]
+    public class ImportAdifViewModelTests
+    {
+        private ImportAdifFromFileViewModel GetViewModel()
+        {
+            return new ImportAdifFromFileViewModel();
+        }
+
+        [TestMethod]
+        public void DuplicateOptionRadioButtonTest()
+        {
+            var model = GetViewModel();
+
+            model.ExecuteAddSelectedAction();
+            Assert.AreEqual(DuplicateRecordSaveOption.Add, model._duplicateRecordSaveOption);
+
+            model.ExecuteOverwriteSelectedAction();
+            Assert.AreEqual(DuplicateRecordSaveOption.Overwrite, model._duplicateRecordSaveOption);
+
+            model.ExecuteIgnoreSelectedAction();
+            Assert.AreEqual(DuplicateRecordSaveOption.Ignore, model._duplicateRecordSaveOption);
+        }
+    }
+}
