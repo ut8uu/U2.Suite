99f3ead18b030d7dc80a76c46c9ce12c3d7e069a
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-04-15 21:52:40 +0300

    Added PowerShell script

--- Sources/run_sonar.cmd
+++
@@ -1,5 +0,0 @@
-dotnet sonarscanner begin /k:"ut8uu_U2.Suite_AYAkh6-fOUOiZYequ8TD" /d:sonar.host.url="http://localhost:9000"  /d:sonar.login="1385879bdd3ceebf2e3b935d03491ecf3e4f403e"
-
-dotnet build
-
-dotnet sonarscanner end /d:sonar.login="1385879bdd3ceebf2e3b935d03491ecf3e4f403e"
