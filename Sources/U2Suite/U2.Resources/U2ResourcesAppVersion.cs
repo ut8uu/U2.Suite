74ca676f419b654f1c41b6b0c8ca99a3ce2c64b5
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-05-04 21:30:38 +0300

    Added license information

--- Sources/U2Suite/U2.Resources/U2ResourcesAppVersion.cs
+++ Sources/U2Suite/U2.Resources/U2ResourcesAppVersion.cs
@@ -1,4 +1,23 @@
-﻿namespace U2.Resources
+﻿/* 
+ * This file is part of the U2.Suite distribution
+ * (https://github.com/ut8uu/U2.Suite).
+ * 
+ * Copyright (c) 2022 Sergey Usmanov, UT8UU.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace U2.Resources
 {
     public sealed class U2ResourcesAppVersion
     {
