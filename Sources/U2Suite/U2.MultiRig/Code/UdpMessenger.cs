a493edadb29ea47b64d4ddf6a2512d65abdfdfce
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-05-07 01:30:39 +0300

    Fixed code of the UdpMessenger

--- Sources/U2Suite/U2.MultiRig/Code/UdpMessenger.cs
+++ Sources/U2Suite/U2.MultiRig/Code/UdpMessenger.cs
@@ -31,33 +31,60 @@ namespace U2.MultiRig;
 public class UdpMessageType
 {
     public const string Status = nameof(Status);
-    public const string Params = nameof(Params);
+    public const string ParameterList = nameof(ParameterList);
+    public const string Parameter = nameof(Parameter);
     public const string Custom = nameof(Custom);
-    public const string Visibility = nameof(Visibility);
+    public const string RigType = nameof(RigType);
     public const string TxQueue = nameof(TxQueue);
 }
 
-public delegate void ApplicationVisibilityEventHandler();
-public delegate void RigStatusCheckEventHandler(int rigNumber);
-public delegate void RigTypeChangedEventHandler(int rigNumber, string newRigType);
-public delegate void RigParametersEventHandler(int rigNumber, int parameters);
-public delegate void RigCustomCommandEventHandler(int rigNumber, byte[] customCommand);
+public delegate void UdpMessageReceivedEventHandler(object sender, string message);
 
-public sealed class UdpMessenger : IDisposable
+public enum UdpMessengerKind
 {
-    private const int TxPort = 11101;
-    private const int RxPort = 11102;
+    Client,
+    Server,
+}
 
+public sealed class UdpClient : UdpMessenger
+{
+    public UdpClient() : base(UdpMessengerKind.Client){}
+}
+
+public sealed class UdpServer : UdpMessenger
+{
+    public UdpServer() : base(UdpMessengerKind.Server){}
+}
+
+public abstract class UdpMessenger : IDisposable
+{
+    public const int ServerTxPort = 11101;
+    public const int ServerRxPort = 11102;
+    
     private readonly UdpEndpoint _udp;
     private readonly ILog _logger = LogManager.GetLogger(typeof(UdpMessenger));
-    private bool _started = false;
+    private bool _started;
 
-    public UdpMessenger()
+    protected UdpMessenger(UdpMessengerKind kind)
     {
-        _udp = new UdpEndpoint(IPAddress.Loopback.ToString(), RxPort);
+        var port = kind == UdpMessengerKind.Server ? ServerRxPort : ServerTxPort;
+        _udp = new UdpEndpoint(IPAddress.Loopback.ToString(), port);
         _udp.EndpointDetected += EndpointDetected;
         _udp.DatagramReceived += DatagramReceived;
-        Start();
+        _udp.Events.Started += Started;
+        _udp.Events.Stopped += Stopped;
+    }
+
+    private void Stopped(object? sender, EventArgs e)
+    {
+        _started = false;
+        _logger.Debug("UdpMessenger stopped.");
+    }
+
+    private void Started(object? sender, EventArgs e)
+    {
+        _started = true;
+        _logger.Debug("UdpMessenger started.");
     }
 
     public void Dispose()
@@ -66,11 +93,7 @@ public sealed class UdpMessenger : IDisposable
         _udp?.Dispose();
     }
 
-    public event ApplicationVisibilityEventHandler? ShowApplication = null;
-    public event RigStatusCheckEventHandler? RigStatusCheck = null;
-    public event RigParametersEventHandler? RigParametersChanged = null;
-    public event RigCustomCommandEventHandler? RigCustomCommand = null;
-    public event RigTypeChangedEventHandler? RigTypeChanged = null;
+    public event UdpMessageReceivedEventHandler? MessageReceived;
 
     public void Start()
     {
@@ -95,66 +118,57 @@ public sealed class UdpMessenger : IDisposable
     private void DatagramReceived(object? sender, Datagram e)
     {
         _logger.Debug($"A datagram received at port {e.Port}: {e.Data}");
+        var s = Encoding.UTF8.GetString(e.Data);
+        if (string.IsNullOrEmpty(s) || !s.StartsWith("MR"))
+        {
+            return;
+        }
+        OnMessageReceived(s);
     }
 
     private void EndpointDetected(object? sender, EndpointMetadata e)
     {
-        throw new NotImplementedException();
+        _logger.Debug($"Endpoint {e.Ip}:{e.Port} detected.");
     }
 
-    private void SendUdpMessage(string key, object param1, object param2)
+    private void SendUdpMessage(int rigNumber, string key, object param1, object param2)
     {
-        var s = $"{key}|{param1}|{param2}";
-        _udp.Send(IPAddress.Loopback.ToString(), TxPort, s);
+        var s = $"MR|{rigNumber}|{key}|{param1}|{param2}";
+        _udp.Send(IPAddress.Loopback.ToString(), ServerTxPort, s);
     }
 
     public void ComNotifyStatus(int rigNumber)
     {
-        SendUdpMessage(UdpMessageType.Status, rigNumber, 0);
+        SendUdpMessage(rigNumber, UdpMessageType.Status, 0, 0);
     }
 
     public void ComNotifyParams(int rigNumber, int parameters)
     {
-        SendUdpMessage(UdpMessageType.Params, rigNumber, parameters);
+        SendUdpMessage(rigNumber, UdpMessageType.ParameterList, parameters, 0);
+    }
+
+    public void ComNotifySingleParameter(int rigNumber, RigParameter parameter, object parameterValue = null)
+    {
+        SendUdpMessage(rigNumber, UdpMessageType.Parameter, (int)parameter, parameterValue);
     }
 
     public void ComNotifyCustom(int rigNumber, object Sender)
     {
-        SendUdpMessage(UdpMessageType.Custom, rigNumber, Convert.ToInt32(Sender));
+        SendUdpMessage(rigNumber, UdpMessageType.Custom, Convert.ToInt32(Sender), 0);
     }
 
     public void TxQueue(int rigNumber)
     {
-        SendUdpMessage(UdpMessageType.TxQueue, rigNumber, 0);
-    }
-
-    private void OnShowApplication()
-    {
-        ShowApplication?.Invoke();
-    }
-
-    private void OnRigStatus(int rigNumber)
-    {
-        RigStatusCheck?.Invoke(rigNumber);
-    }
-
-    private void OnRigParameters(int rigNumber, int parameters)
-    {
-        RigParametersChanged?.Invoke(rigNumber, parameters);
-    }
-
-    private void OnRigCustomCommand(int rigNumber, byte[] customCommand)
-    {
-        RigCustomCommand?.Invoke(rigNumber, customCommand);
-    }
-
-    private void OnRigTypeChanged(int rigNumber, string newRigType)
-    {
-        RigTypeChanged?.Invoke(rigNumber, newRigType);
+        SendUdpMessage(rigNumber, UdpMessageType.TxQueue, 0, 0);
     }
 
     public void ComNotifyRigType(int rigNumber)
     {
         throw new NotImplementedException();
+    }
+
+    private void OnMessageReceived(string message)
+    {
+        MessageReceived?.Invoke(this, message);
     }
 }
