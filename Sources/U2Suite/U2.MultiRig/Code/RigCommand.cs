0155c7d62746cb17a3443ab8e50e55608ce8120a
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-05-21 17:29:38 +0300

    Updated values and comments

--- Sources/U2Suite/U2.MultiRig/Code/RigCommand.cs
+++ Sources/U2Suite/U2.MultiRig/Code/RigCommand.cs
@@ -78,37 +78,37 @@ public struct BitMask
 [Flags]
 public enum RigParameter
 {
-    None = 0,
-    Freq = 1,
-    FreqA = 2,
-    FreqB = 4,
-    Pitch = 8,
-    RitOffset = 16,
-    Rit0 = 32,
-    VfoAA = 64,
-    VfoAB = 128,
-    VfoBA = 256,
-    VfoBB = 512,
-    VfoA = 1024,
-    VfoB = 2048,
-    VfoEqual = 4096,
-    VfoSwap = 8192,
-    SplitOn = 16384,
-    SplitOff = 32768,
-    RitOn = 65536,
-    RitOff = 131072,
-    XitOn = 262144,
-    XitOff = XitOn * 2,
-    Rx = XitOff * 2,
-    Tx = Rx * 2,
-    CW_U = Tx * 2,
-    CW_L = CW_U * 2,
-    SSB_U = CW_L * 2,
-    SSB_L = SSB_U * 2,
-    DIG_U = SSB_L * 2,
-    DIG_L = DIG_U * 2,
-    AM = DIG_L * 2,
-    FM = AM * 2,
+    None = 0,           // 0x00000000
+    Freq = 1,           // 0x00000001
+    FreqA = 2,          // 0x00000002
+    FreqB = 4,          // 0x00000004
+    Pitch = 8,          // 0x00000008
+    RitOffset = 16,     // 0x00000010
+    Rit0 = 32,          // 0x00000020
+    VfoAA = 64,         // 0x00000040
+    VfoAB = 128,        // 0x00000080
+    VfoBA = 256,        // 0x00000100
+    VfoBB = 512,        // 0x00000200
+    VfoA = 1024,        // 0x00000400
+    VfoB = 2048,        // 0x00000800
+    VfoEqual = 4096,    // 0x00001000
+    VfoSwap = 8192,     // 0x00002000
+    SplitOn = 16384,    // 0x00004000
+    SplitOff = 32768,   // 0x00008000
+    RitOn = 65536,      // 0x00010000
+    RitOff = 131072,    // 0x00020000
+    XitOn = 262144,     // 0x00040000
+    XitOff = 524288,    // 0x00080000
+    Rx = 1048576,       // 0x00100000
+    Tx = 2097152,       // 0x00200000
+    CW_U = 4194304,     // 0x00400000
+    CW_L = 8388608,     // 0x00800000
+    SSB_U = 16777216,   // 0x01000000
+    SSB_L = 33554432,   // 0x02000000
+    DIG_U = 67108864,   // 0x04000000
+    DIG_L = 134217728,  // 0x08000000
+    AM = 268435456,     // 0x10000000
+    FM = 536870912,     // 0x20000000
 }
 
 public enum Entry
