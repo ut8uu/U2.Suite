9d6b2312d28a18419c20e46022c72280ca60a867
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-05-25 01:47:30 +0300

    Added unit tests for the reply validation

---
+++ Sources/U2Suite/U2.MultiRig.Tests/HostRigTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace U2.MultiRig.Tests
+{
+    public class HostRigTests
+    {
+        private static HostRig GetHostRig()
+        {
+            return new HostRig(1, KnownIdentifiers.U2MultiRig,
+                new RigSettings(), new RigCommands());
+        }
+
+        [Theory]
+        [ClassData(typeof(HostRigValidateReplyTestData))]
+        public void ValidateReply(HostRigValidateReplyTestDataObject testItem)
+        {
+            var rig = GetHostRig();
+            var isValid = rig.ValidateReply(testItem.Data, testItem.BitMask);
+            Assert.Equal(testItem.IsValid, isValid);
+        }
+    }
+}
---
+++ Sources/U2Suite/U2.MultiRig.Tests/TestData/RigTests/HostRigValidateReplyTestDataObject.cs
@@ -0,0 +1,8 @@
+﻿namespace U2.MultiRig.Tests;
+
+public class HostRigValidateReplyTestDataObject
+{
+    public byte[] Data { get; set; }
+    public BitMask BitMask { get; set; }
+    public bool IsValid { get; set; }
+}
