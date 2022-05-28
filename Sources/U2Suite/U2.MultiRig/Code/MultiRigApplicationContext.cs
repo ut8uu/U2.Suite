b0438208ae62ac718ef03606bac92f388c7ca80e
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-05-29 01:33:03 +0300

    Added IoC application context

---
+++ Sources/U2Suite/U2.MultiRig/Code/MultiRigApplicationContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace U2.MultiRig.Code
+{
+    public sealed class MultiRigApplicationContext
+    {
+        static MultiRigApplicationContext()
+        {
+            Instance = new MultiRigApplicationContext();
+        }
+
+        private MultiRigApplicationContext()
+        {
+            Builder = new ContainerBuilder();
+        }
+
+        public static MultiRigApplicationContext Instance { get; set; }
+
+        public ContainerBuilder Builder { get; }
+        public IContainer Container { get; private set; }
+
+        public void BuildContainer()
+        {
+            Container = Builder.Build();
+        }
+    }
+}
