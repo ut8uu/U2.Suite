02b5cc3e43d88ac3a1be79d77f65231f6726f1dc
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-05-29 01:33:29 +0300

    Added test for IoC

---
+++ Sources/U2Suite/U2.MultiRig.Tests/IoCTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using U2.MultiRig.Code;
+using U2.MultiRig.Utils;
+using Xunit;
+
+namespace U2.MultiRig.Tests;
+public class IoCTests
+{
+    [Fact]
+    public void TestRegistration()
+    {
+        var builder = new ContainerBuilder();
+        builder.Register(c => new IcomIC705SerialPortEmulator())
+            .As<IRigSerialPort>();
+
+        var container = builder.Build();
+
+        var serialPort = container.Resolve<IRigSerialPort>();
+        Assert.NotNull(serialPort);
+        Assert.Equal(typeof(IcomIC705SerialPortEmulator), serialPort.GetType());
+    }
+}
