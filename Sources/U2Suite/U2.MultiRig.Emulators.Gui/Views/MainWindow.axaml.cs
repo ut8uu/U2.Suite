d299bf18e2dd42619d7609214310ced31cd4cb1f
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-06-09 00:13:47 +0300

    Added a GUI application for emulators

---
+++ Sources/U2Suite/U2.MultiRig.Emulators.Gui/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace U2.MultiRig.Emulators.Gui.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
