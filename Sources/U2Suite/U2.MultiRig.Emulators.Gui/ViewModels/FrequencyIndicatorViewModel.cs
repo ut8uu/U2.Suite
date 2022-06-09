de8df7f64b6a5e895fad43a038e7503bdc1c0451
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-06-09 17:25:48 +0300

    Implemented FrequencyIndicator user control

---
+++ Sources/U2Suite/U2.MultiRig.Emulators.Gui/ViewModels/FrequencyIndicatorViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using ReactiveUI.Fody.Helpers;
+
+namespace U2.MultiRig.Emulators.Gui;
+
+public class FrequencyIndicatorViewModel
+{
+    private UserControl _owner;
+    private readonly List<UpDownDigit> _digits = new();
+    private int _frequency;
+
+    public UserControl Owner
+    {
+        get => _owner;
+        set
+        {
+            _owner = value;
+
+            _digits.Clear();
+            for (var i = 0; i < 10; i++)
+            {
+                var control = _owner.FindControl<UpDownDigit>($"Digit{i}");
+                Debug.Assert(control != null, $"An UpDownDigit control Digit{i} not found.");
+                control.ValueChanged += ControlOnFrequencyChanged;
+                _digits.Add(control);
+            }
+        }
+    }
+
+    private void ControlOnFrequencyChanged(object sender, ValueChangedEventArgs eventArgs)
+    {
+        Frequency = eventArgs.Value;
+    }
+
+    [Reactive]
+    public int Frequency
+    {
+        get => _frequency;
+        set
+        {
+            _frequency = value;
+            foreach (var digit in _digits)
+            {
+                digit.Frequency = value;
+            }
+        }
+    }
+
+    [Reactive] 
+    public int Width { get; set; } = 450;
+
+    public void ValueChanged(object sender, ValueChangedEventArgs eventArgs)
+    {
+        Frequency = eventArgs.Value;
+    }
+}
+
+public class DemoFrequencyIndicatorViewModel : FrequencyIndicatorViewModel
+{
+    public DemoFrequencyIndicatorViewModel()
+    {
+        Frequency = 1234567890;
+    }
+}
--- Sources/U2Suite/U2.MultiRig.Emulators.Gui/Views/MainWindow.axaml.cs
+++ Sources/U2Suite/U2.MultiRig.Emulators.Gui/Views/MainWindow.axaml.cs
@@ -1,12 +1,36 @@
+/* 
+ * This file is part of the U2.Suite distribution
+ * (https://github.com/ut8uu/U2.Suite).
+ * 
+ * Copyright (c) 2022 Sergey Usmanov, UT8UU.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 using Avalonia.Controls;
 
-namespace U2.MultiRig.Emulators.Gui.Views
+namespace U2.MultiRig.Emulators.Gui;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    private readonly MainWindowViewModel _viewModel;
+
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+
+        _viewModel = new MainWindowViewModel();
+        DataContext = _viewModel;
+        _viewModel.Owner = this;
     }
 }
