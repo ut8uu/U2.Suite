cbd7a4b13f274de7e940a2f197329e94ba21dd10
Sergey Usmanov <ut8uu@ukr.net>
2023-04-14 23:02:24 +0300

    Added U2.Logger.Core project

---
+++ Sources/U2.LoggerSvc.Core/Exceptions/ContactCreationFailedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U2.LoggerSvc.Core;
+
+public sealed class ContactCreationFailedException : LoggerException
+{
+    public ContactCreationFailedException(string message) : base(message) { }
+}
---
+++ Sources/U2.LoggerSvc.Core/Interfaces/ILoggerService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using U2.LoggerSvc.Core;
+
+namespace U2.LoggerSvc.Core;
+
+public interface ILoggerService
+{
+    /// <summary>
+    /// Retrieves all contacts from the database.
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<IEnumerable<Contact>> GetContactsAsync(CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Creates a new contact using given data.
+    /// </summary>
+    /// <param name="contact">A contact to be created.</param>
+    /// <param name="cancellationToken"></param>
+    /// <exception cref="ContactCreationFailedException"></exception>
+    Task CreateAsync(Contact contact, CancellationToken cancellationToken);
+}
