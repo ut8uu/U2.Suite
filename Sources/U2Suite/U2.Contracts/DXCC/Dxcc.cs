830752fa8e49a9b5fb96cb0bddbce17c3d31356b
Sergey Usmanov <ut8uu@ukr.net>
2023-04-15 21:34:28 +0300

    Added dxcc and test

---
+++ Sources/U2Suite/U2.Contracts/DXCC/Dxcc.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace U2.Contracts;
+
+public class Dxcc
+{
+    [JsonProperty("continent")]
+    public List<string> Continent { get; set; }
+
+    [JsonProperty("countryCode")]
+    public string CountryCode { get; set; }
+
+    [JsonProperty("cq")]
+    public List<int> Cq { get; set; }
+
+    [JsonProperty("deleted")]
+    public bool Deleted { get; set; }
+
+    [JsonProperty("entityCode")]
+    public int EntityCode { get; set; }
+
+    [JsonProperty("flag")]
+    public string Flag { get; set; }
+
+    [JsonProperty("itu")]
+    public List<int> Itu { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("notes")]
+    public string Notes { get; set; }
+
+    [JsonProperty("outgoingQslService")]
+    public bool OutgoingQslService { get; set; }
+
+    [JsonProperty("prefix")]
+    public string Prefix { get; set; }
+
+    [JsonProperty("prefixRegex")]
+    public string PrefixRegex { get; set; }
+
+    [JsonProperty("thirdPartyTraffic")]
+    public bool ThirdPartyTraffic { get; set; }
+
+    [JsonProperty("validEnd")]
+    public string ValidEnd { get; set; }
+
+    [JsonProperty("validStart")]
+    public string ValidStart { get; set; }
+}
---
+++ Sources/U2Suite/U2.Contracts/DXCC/DxccList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace U2.Contracts;
+
+public class DxccList : List<Dxcc>
+{
+    public static DxccList FromFile(string path)
+    {
+        Debug.Assert(path != null);
+        Debug.Assert(File.Exists(path));
+
+        using var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Delete);
+        var result = JsonSerializer.Deserialize<DxccList>(fs);
+        return result;
+    }
+
+    public static DxccList FromString(string json)
+    {
+        using var ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
+        var result = JsonSerializer.Deserialize<DxccList>(ms);
+        return result;
+    }
+}
