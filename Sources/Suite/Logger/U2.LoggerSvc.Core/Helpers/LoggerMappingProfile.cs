910139f92af1b559712fc5b88008afefde5337c8
Sergey Usmanov <ut8uu@ukr.net>
2023-04-21 23:19:22 +0300

    Implemented new mappings

--- Sources/Suite/Logger/U2.LoggerSvc.Core/Helpers/LoggerMappingProfile.cs
+++ Sources/Suite/Logger/U2.LoggerSvc.Core/Helpers/LoggerMappingProfile.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using U2.Core;
+using U2.LoggerSvc.ApiTypes.v1;
 using U2.LoggerSvc.Data;
 
 namespace U2.LoggerSvc.Core;
@@ -12,7 +14,26 @@ public class LoggerMappingProfile : Profile
 {
     public LoggerMappingProfile()
     {
-        CreateMap<LogEntry, Contact>();
-        CreateMap<Contact, LogEntry>();
+        CreateMap<Contact, ContactDto>()
+            .ForMember(dest => dest.Band, act => act.MapFrom(src => src.Band.Name))
+            .ForMember(dest => dest.BandRx, act => act.MapFrom(src => src.BandRx.Name))
+            .ForMember(dest => dest.Mode, act => act.MapFrom(src => src.Mode.Name))
+            ;
+        CreateMap<ContactDto, Contact>()
+            .ForMember(dest => dest.Band, act => act.MapFrom(src => ConversionHelper.BandNameToBand(src.Band)))
+            .ForMember(dest => dest.BandRx, act => act.MapFrom(src => ConversionHelper.BandNameToBand(src.BandRx ?? src.Band)))
+            .ForMember(dest => dest.Mode, act => act.MapFrom(src => ConversionHelper.ModeNameToMode(src.Mode)))
+            ;
+
+        CreateMap<Contact, LogEntry>()
+            .ForMember(dest => dest.Band, act => act.MapFrom(src => src.Band.Name))
+            .ForMember(dest => dest.BandRx, act => act.MapFrom(src => src.BandRx.Name))
+            .ForMember(dest => dest.Mode, act => act.MapFrom(src => src.Mode.Name))
+            ;
+        CreateMap<LogEntry, Contact>()
+            .ForMember(dest => dest.Band, act => act.MapFrom(src => ConversionHelper.BandNameToBand(src.Band)))
+            .ForMember(dest => dest.BandRx, act => act.MapFrom(src => ConversionHelper.BandNameToBand(src.BandRx ?? src.Band)))
+            .ForMember(dest => dest.Mode, act => act.MapFrom(src => ConversionHelper.ModeNameToMode(src.Mode)))
+            ;
     }
 }
