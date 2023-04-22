9d4b667dc10681c3a9f2d23e4a5c973d902815cb
Sergey Usmanov <ut8uu@ukr.net>
2023-04-22 23:35:18 +0300

    Added new exceptions

---
+++ Sources/Suite/Logger/U2.LoggerSvc.Core/Exceptions/ContactUpdateFailedException.cs
@@ -0,0 +1,8 @@
+﻿namespace U2.LoggerSvc.Core;
+
+public sealed class ContactUpdateFailedException : LoggerException
+{
+    public ContactUpdateFailedException(int id, Exception? innerException = null) :
+        base($"Updating of the contact with id='{id}' failed.", innerException)
+    { }
+}
