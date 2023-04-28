678bff0b3d3f59ab211e111c739b44de13c0add4
Sergey Usmanov <ut8uu@ukr.net>
2023-04-28 23:39:31 +0300

    Implemented displaying of the call info

--- Sources/Suite/Logger/U2.Logger.WebApp/Controllers/CisController.cs
+++ Sources/Suite/Logger/U2.Logger.WebApp/Controllers/CisController.cs
@@ -4,17 +4,27 @@ namespace U2.Logger.WebApp;
 
 public class CisController : Controller
 {
-    public IActionResult Index(
-        [FromQuery] int pageIndex = 0, 
-        [FromQuery] string searchString = "",
-        [FromQuery] string call = "")
-    {
-        var cisPage = new CisPage(pageIndex, searchString, call);
-
-        cisPage.Calls = new CisCallList
-        {
-            new CisCallShort(1, "UT8UU"),
-        };
-        return View(cisPage);
-    }
+	public IActionResult Index(
+		[FromQuery] int pageIndex = 0,
+		[FromQuery] string searchString = "",
+		[FromQuery] string call = "")
+	{
+		var cisPage = new CisPage(pageIndex, searchString, call)
+		{
+			Calls = new CisCallList
+			{
+				new CisCallShort(1, "UT8UU"),
+				new CisCallShort(2, "UT2UU"),
+				new CisCallShort(3, "ZA7UU"),
+			},
+			CallInfo = new CisCallInfo
+			{
+				Id = 1,
+				Call = "UT8UU",
+				FirstName = "Sergey",
+				LastName = "Usmanov",
+			},
+		};
+		return View(cisPage);
+	}
 }
