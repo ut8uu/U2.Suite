f959b38b752cf680d97b5fe47af5aaa16b43660e
Sergey Usmanov <ut8uu@ukr.net>
2023-04-29 12:15:18 +0300

    Implemented displaying of the call information

---
+++ Sources/Suite/Logger/U2.Logger.WebApp/Models/Cis/CallInfoRow.cs
@@ -0,0 +1,17 @@
+﻿namespace U2.Logger.WebApp;
+
+#nullable disable
+
+public class CallInfoRow
+{
+    public bool IsCaption { get; set; }
+    public string Title { get; set; }
+	public string Value { get; set; }
+
+	public CallInfoRow(string title, string value, bool isCaption = false)
+	{
+		Title = title;
+		Value = value;
+		IsCaption = isCaption;
+	}
+}
--- Sources/Suite/Logger/U2.Logger.WebApp/Models/Cis/CisCallInfo.cs
+++ Sources/Suite/Logger/U2.Logger.WebApp/Models/Cis/CisCallInfo.cs
@@ -4,9 +4,21 @@
 
 public class CisCallInfo
 {
-    public int Id { get; set; }
-    public string Call { get; set; }
-    public string Aliases { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+	public int Id { get; set; }
+	public string Call { get; set; }
+	public string Aliases { get; set; }
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+
+	public IEnumerable<CallInfoRow> GetCallInfoRows()
+	{
+		return new List<CallInfoRow>
+		{
+			new CallInfoRow("General Info", string.Empty, isCaption: true),
+			new CallInfoRow("Call", Call),
+			new CallInfoRow("Aliases", Aliases),
+			new CallInfoRow("First Name", FirstName),
+			new CallInfoRow("Last Name", LastName),
+		};
+	}
 }
