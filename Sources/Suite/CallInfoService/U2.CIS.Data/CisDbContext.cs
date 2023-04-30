9b155cf41ec48a5b4e219caf5003d783b00ea32e
Sergey Usmanov <ut8uu@ukr.net>
2023-04-30 14:47:14 +0300

    Added data context and migration

--- Sources/Suite/CallInfoService/U2.CIS.Data/CisDbContext.cs
+++ Sources/Suite/CallInfoService/U2.CIS.Data/CisDbContext.cs
@@ -1,8 +1,55 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using U2.Core;
 
 namespace U2.CIS.Data;
 
 public class CisDbContext : DbContext, ICisDbContext
 {
+	public const string DefaultDatabaseName = "call_info_service.sqlite";
 
+	private readonly string _dbName = DefaultDatabaseName;
+	protected readonly string _dbPath;
+
+	public CisDbContext(string dbPath = "", string dbName = DefaultDatabaseName)
+	{
+		if (dbPath.Length > 0 && Directory.Exists(dbPath))
+		{
+			_dbPath = dbPath;
+		}
+		else
+		{
+			_dbPath = FileSystemHelper.GetDatabaseFolderPath(applicationName: "CIS");
+		}
+		Directory.CreateDirectory(_dbPath);
+		_dbName = dbName;
+	}
+
+	public string DbPath => Path.Combine(_dbPath, _dbName);
+
+	public string DbName => _dbName;
+
+	public DbSet<CallInfoEntry> Entries { get; set; }
+
+	protected override void OnConfiguring(DbContextOptionsBuilder options)
+		=> options.UseSqlite($"Data Source={DbPath}");
+
+	public Task<int> AddCallInfoEntryAsync(CallInfoEntry entry, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<bool> DeleteCallInfoEntryAsync(int id, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<CallInfoEntry> GetCallInfoEntryAsync(string call, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<bool> UpdateCallInfoEntryAsync(CallInfoEntry entry, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
 }
---
+++ Sources/Suite/CallInfoService/U2.CIS.Data/Migrations/20230430105459_InitialMigration.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace U2.CIS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Call = table.Column<string>(type: "TEXT", nullable: true),
+                    Aliases = table.Column<string>(type: "TEXT", nullable: true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: true),
+                    LastName = table.Column<string>(type: "TEXT", nullable: true),
+                    YearOfBirth = table.Column<int>(type: "INTEGER", nullable: true),
+                    EMail = table.Column<string>(type: "TEXT", nullable: true),
+                    AddressLine1 = table.Column<string>(type: "TEXT", nullable: true),
+                    AddressLine2 = table.Column<string>(type: "TEXT", nullable: true),
+                    State = table.Column<string>(type: "TEXT", nullable: true),
+                    DxccCountry = table.Column<string>(type: "TEXT", nullable: true),
+                    Country = table.Column<string>(type: "TEXT", nullable: true),
+                    ZIP = table.Column<string>(type: "TEXT", nullable: true),
+                    GridLocator = table.Column<string>(type: "TEXT", nullable: true),
+                    Latitude = table.Column<decimal>(type: "TEXT", nullable: true),
+                    Longitude = table.Column<decimal>(type: "TEXT", nullable: true),
+                    CqZone = table.Column<int>(type: "INTEGER", nullable: true),
+                    ItuZone = table.Column<int>(type: "INTEGER", nullable: true),
+                    QslManager = table.Column<string>(type: "TEXT", nullable: true),
+                    AcceptsEqsl = table.Column<bool>(type: "INTEGER", nullable: true),
+                    AcceptsLotw = table.Column<bool>(type: "INTEGER", nullable: true),
+                    UsesPaperQsl = table.Column<bool>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entries", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Entries");
+        }
+    }
+}
