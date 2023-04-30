70c557549b7fea47989b791374fbb374229a2d56
Sergey Usmanov <ut8uu@ukr.net>
2023-04-30 14:47:55 +0300

    Added CIS-related exceptions

---
+++ Sources/Suite/CallInfoService/U2.CIS.Core/Exceptions/CallDeleteFailedException.cs
@@ -0,0 +1,12 @@
+﻿namespace U2.CIS.Core;
+
+/// <summary>
+/// Represents an exception raised during the deletion of the call from the database.
+/// </summary>
+public class CallDeleteFailedException : CisException
+{
+	public CallDeleteFailedException(string? message, Exception? innerException)
+		: base(message, innerException)
+	{
+	}
+}
---
+++ Sources/Suite/CallInfoService/U2.CIS.Core/Exceptions/CisException.cs
@@ -0,0 +1,9 @@
+﻿namespace U2.CIS.Core;
+
+public class CisException : Exception
+{
+	public CisException(string? message, Exception? innerException) 
+		: base(message, innerException)
+	{
+	}
+}
