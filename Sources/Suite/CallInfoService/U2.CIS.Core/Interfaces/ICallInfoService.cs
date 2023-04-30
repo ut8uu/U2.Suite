8b808a681dfb68863fda22560f469b4a794fa070
Sergey Usmanov <ut8uu@ukr.net>
2023-04-30 14:48:25 +0300

    Added CallInfoService

---
+++ Sources/Suite/CallInfoService/U2.CIS.Core/Interfaces/ICallInfoService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace U2.CIS.Core;
+
+public interface ICallInfoService
+{
+	/// <summary>
+	/// Adds given call to the database.
+	/// </summary>
+	/// <param name="call"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	/// <exception cref="CallAddFailedException"></exception>
+	Task AddCallAsync(string call, CancellationToken cancellationToken);
+
+	/// <summary>
+	/// Deletes given call from the database.
+	/// </summary>
+	/// <param name="call"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	/// <exception cref="CallDeleteFailedException"></exception>
+	Task DeleteCallAsync(string call, CancellationToken cancellationToken);
+
+	/// <summary>
+	/// Deletes given call from the database.
+	/// </summary>
+	/// <param name="call"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	/// <exception cref="CallDeleteFailedException"></exception>
+	Task UpdateCallAsync(string call, CallInfo callInfo, CancellationToken cancellationToken);
+}
---
+++ Sources/Suite/CallInfoService/U2.CIS.Core/Services/CallInfoService.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace U2.CIS.Core;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+public sealed class CallInfoService : ICallInfoService
+{
+	public CallInfoService()
+	{
+	}
+
+	public Task AddCallAsync(string call, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task DeleteCallAsync(string call, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task UpdateCallAsync(string call, CallInfo callInfo, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+
+	private string GetDebuggerDisplay()
+	{
+		return ToString();
+	}
+}
